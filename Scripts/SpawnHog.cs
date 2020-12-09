55ceb510bbc641aad215b4c5623e96a8917b4ab0
ComradeCrunchy <49103834+ComradeCrunchy@users.noreply.github.com>
2020-12-09 08:15:56 -0500

    scripts
    scripts commit

---
+++ Scripts/SpawnHog.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnHog : MonoBehaviour
+{
+    public GameObject hog;
+    public GameObject camera;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Vector3 pos = new Vector3(10,10,10);
+            GameObject c = Instantiate(hog, pos, Quaternion.identity);
+            camera.GetComponent<SmoothFollow>().target = c.transform;
+        }
+    }
+}
---
+++ Scripts/SpawnProxy.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Entities;
+
+public class SpawnProxy : MonoBehaviour, IDeclareReferencedPrefabs, IConvertGameObjectToEntity
+{
+    public GameObject cube;
+    public int rows;
+    public int cols;
+
+    public void DeclareReferencedPrefabs(List<GameObject> gameObjects)
+    {
+        gameObjects.Add(cube);
+    }
+
+    public void Convert(Entity entity, EntityManager dstManager, GameObjectConversionSystem conversionSystem)
+    {
+        var spawnData = new Spawner
+        {
+            Prefab = conversionSystem.GetPrimaryEntity(cube),
+            Erows = rows,
+            Ecols = cols
+        };
+        dstManager.AddComponentData(entity, spawnData);
+    }
+}
